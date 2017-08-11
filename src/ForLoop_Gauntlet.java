05d39896b013a70d07af2d6c515ab4693c867a31
najclark <league@WTS10.attlocal.net>
2017-08-11 14:26:54 -0700

    Day 5

---
+++ src/ForLoop_Gauntlet.java
@@ -0,0 +1,48 @@
+
+public class ForLoop_Gauntlet {
+public static void main(String[] args) {
+	//Problems not listed already completed
+	//Problem 5
+	//"syso" (Shortcut) equals print in console
+	//Variable%Variable equals Variable divide by Variable	
+for (int i = 1; i < 501; i++) {
+if (i%2==0) {
+	System.out.println(i+ " even");
+}
+else{
+System.out.println(i + " odd");
+}
+
+}	
+	//Problem 7
+int age=2004;
+for (int i = 0; i < 13; i++) {
+	System.out.println("In "+age++ + ", I was "+i+" years old");
+
+
+}
+for (int i = 0; i < 3; i++) {
+for (int j = 0; j < 3; j++) {
+	
+
+	System.out.println(i+" "+j);	
+
+
+
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+}
